155ecb12c5b109511c0ff041b438e19ad20f0e4e
kamunikan <109719587+kamunikan@users.noreply.github.com>
2022-09-05 11:08:43 +0800

    Update install_up.sh

--- install_up.sh
+++ install_up.sh
@@ -10,5 +10,13 @@ export COLOR1="$(cat /etc/scvpn/theme/$colornow | grep -w "TEXT" | cut -d: -f2|s
 export COLBG1="$(cat /etc/scvpn/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"                    
 ###########- END COLOR CODE -##########
 
-echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Downloading Update File"
-sleep 4
+echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Remove Old Script"
+# rm /usr/bin/filename
+sleep 2
+echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Downloading New Script"
+# wget -q -O /usr/bin/FILENAME "https://raw.githubusercontent.com/kamunikan/update/main/update_file/FILENAME" && chmod +x /usr/bin/FILENAME
+sleep 2
+echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Download Changelog File"
+wget -q -O /root/clog.txt "https://raw.githubusercontent.com/kamunikan/update/main/update_file/clog.txt" && chmod +x /root/clog.txt
+echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Read Clog? ./root/clog.txt"
+sleep 2
